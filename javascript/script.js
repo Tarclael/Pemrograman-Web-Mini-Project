c23b7800e7467f326715f8292115735dc0a07c9e
Patrick Kocu <tarclael5@gmail.com>
2025-09-25 06:46:06 +0700

    making drop down nav menus & fixing home page small screen

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+const burgerIcon = document.querySelector('#burgerIcon');
+const closeIcon = document.querySelector('#closeIcon');
+const menu = document.querySelector('#menu');
+
+burgerIcon.addEventListener('click', () => {
+    menu.classList.remove('h-0');
+    menu.classList.add('h-[80%]');  // expand to 80%
+    burgerIcon.classList.add('hidden');
+    closeIcon.classList.remove('hidden');
+});
+
+closeIcon.addEventListener('click', () => {
+    menu.classList.remove('h-[80%]');
+    menu.classList.add('h-0');      // collapse back
+    closeIcon.classList.add('hidden');
+    burgerIcon.classList.remove('hidden');
+});
