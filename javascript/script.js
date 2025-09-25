0ad59e4c9dacc9e044170408859d698537f0a5e6
Patrick Kocu <tarclael5@gmail.com>
2025-09-25 10:38:09 +0700

    adding nav option underlining

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,7 @@ const burgerIcon = document.querySelector('#burgerIcon');
 const closeIcon = document.querySelector('#closeIcon');
 const menu = document.querySelector('#menu');
 
+// open menus
 burgerIcon.addEventListener('click', () => {
     menu.classList.remove('h-0');
     menu.classList.add('h-[80%]');  // expand to 80%
@@ -9,6 +10,7 @@ burgerIcon.addEventListener('click', () => {
     closeIcon.classList.remove('hidden');
 });
 
+// close menus
 closeIcon.addEventListener('click', () => {
     menu.classList.remove('h-[80%]');
     menu.classList.add('h-0');      // collapse back
